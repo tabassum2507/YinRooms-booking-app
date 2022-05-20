63f5eed95f519cacf815633efb410760c99acba7
tabassum2507 <10tabassum2@gmail.com>
2022-05-21 00:38:32 +0530

    mail added

---
+++ src/components/mailList/MailList.jsx
@@ -0,0 +1,20 @@
+import './mailList.css'
+
+const MailList = () => {
+  return (
+    <div className='mail'>
+        <h1 className="mailTitle">
+            Save time, save money!
+        </h1>
+        <span className="mailDesc">
+            Sign Up and we'll send you the best details
+        </span>
+        <div className="mailInputContainer">
+        <input type="text" placeholder='Your Email...' />
+        <button>Subscribe</button>
+        </div>
+    </div>
+  )
+}
+
+export default MailList
