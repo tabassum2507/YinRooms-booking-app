690bffd3d8a0484846c8340f0badb74bca0fbdf9
tabassum2507 <10tabassum2@gmail.com>
2022-05-20 19:04:35 +0530

    featured properties added

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,57 @@
+import './propertyList.css'
+
+const PropertyList = () => {
+  return (
+    <div className='pList'>
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1562809896-1b1514f40708?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=581" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>700+ Hotels</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1529703413164-adb3026bb1e6?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=589" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Cabins</h1>
+                <h2>200+ Cabins</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1584008186374-977c41c84b43?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Villas</h1>
+                <h2>500+ Villas</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1567051220345-0b1f7f74e8a7?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Resorts</h1>
+                <h2>300+ Resorts</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1632400990400-416d5460f337?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=618" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Apartments</h1>
+                <h2>600+ Apartments</h2>
+            </div>
+        </div>
+
+        <div className="pListItem">
+            <img src="https://images.unsplash.com/photo-1549294413-26f195200c16?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464" alt="" className="pListImg" />
+            <div className="pListTitles">
+                <h1>Hotels</h1>
+                <h2>700+ Hotels</h2>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default PropertyList
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProjects/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
 import './home.css'
 
 export default function Home() {
@@ -10,6 +12,10 @@ export default function Home() {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitles">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
